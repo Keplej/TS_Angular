5d4cd6556d9ac344636e750a236093cd223229fc
Keplej <Johnny.Keple@gmail.com>
2024-09-13 14:15:48 -0500

    Adding pagination component

--- demo_project/src/app/home/home.component.ts
+++ demo_project/src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from "../components/product/product.component";
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -20,17 +21,37 @@ export class HomeComponent {
   
   products: Product[] = [];
 
+  totalRecords: number = 0;
+  rows: number = 5;
+
   onProductOutput(product: Product) {
     console.log(product, "Output");
   }
 
+  // When we change the page and event will be emitted to our custom function so we fetch the page and rows
+  onPageChange(event:any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+  
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
+      .subscribe((prodcuts: Products) => {
+        this.products = prodcuts.items;
+        this.totalRecords = prodcuts.total;
+      });
+  }
+
   ngOnInit() {
     // called when inilized component
-    this.productsService
-      .getProducts('http://localhost:3000/clothes', {page: 0, perPage:5})
-      .subscribe((products: Products) => {
-        // console.log(products.items);
-        this.products = products.items;
-    })
+    // this.productsService
+    //   .getProducts('http://localhost:3000/clothes', {page: 0, perPage:5})
+    //   .subscribe((products: Products) => {
+    //     // console.log(products.items);
+    //     this.products = products.items;
+    // })
+
+    this.fetchProducts(0, this.rows); // this is bascially selecting from fetchProducts and we input the page and perPage. So page is 0 and perPage is 5
   }
 }
