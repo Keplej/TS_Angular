f7a13f80a37a751775fd63b84b8fd6725d8db2c4
Keplej <Johnny.Keple@gmail.com>
2024-09-17 01:41:03 -0500

    Adding Example about us lazy routing

--- demo_project/src/app/app.routes.ts
+++ demo_project/src/app/app.routes.ts
@@ -6,4 +6,12 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent,
     },
+    {
+        // This is how we lazy load a module accessing a specific route
+        path: 'about-us',
+        loadChildren: () => 
+            import('./modules/about-us/about-us.module').then(
+                (m) => m.AboutUsModule
+            ),
+    }
 ];
